13a6a92ed678cdd9d9b3843baeb75fc1b3b90b30
SimonCZW <815490748@qq.com>
2016-10-10 20:46:31 +0800

    Basic models of my blog

---
+++ blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from blog.models import Paper, Category
+
+admin.site.register(Paper)
+admin.site.register(Category)
+
+# Register your models here.
---
+++ blog/models.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+
+@python_2_unicode_compatible
+class Category(models.Model):
+    name = models.CharField('NAME', max_length=32)
+    
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
+class Paper(models.Model):
+    title = models.CharField('TITLE', max_length=32)
+    content = models.TextField('CONTENT')
+    created_time = models.DateTimeField('CREATED_TIME', auto_now_add=True)
+    update_time = models.DateTimeField('UPDATE_TIME', auto_now=True)
+    author = models.CharField('AUTHOR', max_length=32)
+    category = models.ManyToManyField(Category, blank=True)
+
+    def __str__(self):
+        return self.title
+
