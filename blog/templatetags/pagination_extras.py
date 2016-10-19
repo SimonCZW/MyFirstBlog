48e0b947fa05f328c56a4ffdd0f42a948118d0bb
SimonCZW <815490748@qq.com>
2016-10-19 21:11:10 +0800

    pagination second version

--- blog/templatetags/pagination_extras.py
+++ blog/templatetags/pagination_extras.py
@@ -1,5 +1,6 @@
 from django import template
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 #django.template.Library.filter() for register function as filter
 register = template.Library()
 
@@ -9,30 +10,36 @@ def pagination(context, paperList, pageNum):
     page = context['request'].GET.get('page')
     try:
         contacts = paginator.page(page)
+        context['current_page'] = page
     except PageNotAnInteger:
         contacts = paginator.page(1)
+        context['current_page'] = 1
     except EmptyPage:
         contacts = paginator.page(paginator.num_pages)
+
+    context['total_page'] = range(1,paginator.num_pages+1) 
     context['contacts'] = contacts
+
     return ''
 
+
+
+
+###################Learn Custom Template filter/tags#####################
 @register.filter
 def lower(value):
     return value.lower()
-
 @register.simple_tag(takes_context=True)
 def my_tag(context, var):
     #context['string'] = string.lower()
     context['string'] = var.upper()
     #return var.lower() 
     return ''
-
 @register.tag
 def upper(parser, token):
     nodelist = parser.parse(('endupper',))
     parser.delete_first_token()
     return UpperNode(nodelist)
-
 class UpperNode(template.Node):
     def __init__(self, nodelist):
         self.nodelist = nodelist
