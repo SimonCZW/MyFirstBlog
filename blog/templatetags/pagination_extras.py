d685f18c1ed8a8d5458d536130fd57247e154ff0
SimonCZW <815490748@qq.com>
2016-10-23 01:43:23 +0800

    Pagination Second Version: list left directory

--- blog/templatetags/pagination_extras.py
+++ blog/templatetags/pagination_extras.py
@@ -8,6 +8,7 @@ register = template.Library()
 def pagination(context, paperList, pageNum):
     paginator = Paginator(paperList, pageNum)
     page = context['request'].GET.get('page')
+
     try:
         contacts = paginator.page(page)
         context['current_page'] = int(page)
@@ -19,14 +20,51 @@ def pagination(context, paperList, pageNum):
 
     context['first_page'] = 1
     context['last_page'] = paginator.num_pages
-
     context['total_page'] = range(1,paginator.num_pages+1) 
     context['contacts'] = contacts
 
+    limit_length = 2
+    #get left page list
+    try:
+        context['left_pages'], context['LeftMoreTag'] = get_left(int(page), limit_length)
+    except:
+        context['left_pages'], context['LeftMoreTag'] = get_left(1, limit_length)
+
+
+#    context['right_pages'] = get_right(int(page), 2)
     return ''
 
+#return left page list
+def get_left(current_page, limit_page):
+    #judge limit_page len
+    #further more
+    if len(range(2, current_page)) > limit_page:
+        LeftPageList = range(2, current_page+1)[-1-limit_page:-1] 
+        LeftMoreTag = True  
+    
+    else:
+        LeftPageList = range(2, current_page)
+        LeftMoreTag = False
 
+    return LeftPageList, LeftMoreTag
+    
+    ##perfectly
+    #elif len(range(2, current_page)) == limit_page:
+    #    LeftPageList = range(2, current_page)
+    #    MoreTag = False
+    ##just 2
+    #elif len(range(2, current_page)) == 0:
+    #    LeftPageList = []
+    #    MoreTag = False
+    #else:
+    #    LeftPageList = range(2, current_page)
+    #    MoreTag = False
+    
 
+#return right page list
+#def get_right(current_page, limit_page):
+#
+#    return LeftPageList
 
 
 
