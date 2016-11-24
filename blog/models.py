f9f874f13f06978d24fe1e3e2f195405a5912ed3
SimonCZW <815490748@qq.com>
2016-11-24 04:07:31 -0800

    add paper click_counter by adding a field in Paper Class

--- blog/models.py
+++ blog/models.py
@@ -26,14 +26,15 @@ class Tag(models.Model):
 @python_2_unicode_compatible
 class Paper(models.Model):
     title = models.CharField('TITLE', max_length=32)
-    abstract = models.CharField('ABSTRACT', max_length=256, null=True)
+    abstract = models.CharField('ABSTRACT', max_length=256)
     content = models.TextField('CONTENT')
     created_time = models.DateTimeField('CREATED_TIME', auto_now_add=True)
     update_time = models.DateTimeField('UPDATE_TIME', auto_now=True)
     author = models.CharField('AUTHOR', max_length=32)
-    category = models.ManyToManyField(Category, blank=True)
-    images = models.ImageField(upload_to='photos',null=True,blank=True)
+    category = models.ForeignKey(Category, blank=True)
+    images = models.ImageField(upload_to='photos', null=True,blank=True)
     tags = models.ManyToManyField(Tag, null=True, blank=True)
+    click = models.IntegerField(default=0)
 
     def get_absolute_url(self):
         return reverse('blog:paper', kwargs={'paper_id':self.pk})
